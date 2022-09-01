078b268c67bcaf7cd2aa28ebda54f5a9a7f51828
Job <0326531@student.rocvantwente.nl>
2022-09-01 10:06:24 +0200

    puck hit effects
    nog een effect op dit moment, meer word aan gewerkt

---
+++ Assets/PuckHitEffects.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuckHitEffects : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject effectPrefab;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Floor") || collision.transform.childCount > 14)
+            return;
+        GameObject e;
+        Destroy(e = Instantiate(effectPrefab, collision.collider.ClosestPoint(transform.position), Quaternion.identity), 5);
+        e.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+        e.transform.parent = collision.transform;
+    }
+}
+/*
+        random rotation:
+
+        var euler = transform.eulerAngles;
+        euler.y = Random.Range(-0.9f, 0.9f);
+        e.transform.eulerAngles = euler;
+
+ */
