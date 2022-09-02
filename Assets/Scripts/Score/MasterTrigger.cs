ee09fe35960b547dfac6c0c14c6ed6b80463c830
Dion Vos <dionvos160@gmail.com>
2022-09-02 09:45:17 +0200

    Score Better and implemented

---
+++ Assets/Scripts/Score/MasterTrigger.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterTrigger : MonoBehaviour
+{
+    public Text txt;
+    private void OnTriggerEnter(Collider other)
+    {
+        Calculate();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Calculate();
+    }
+    public void Calculate()
+    {
+        int score = 0;
+        int scoreOne = GameObject.Find("Point 1 Trigger").GetComponent<PointTrigger>().score;
+        int scoreTwo = GameObject.Find("Point 2 Trigger").GetComponent<PointTrigger>().score;
+        int scoreThree = GameObject.Find("Point 3 Trigger").GetComponent<PointTrigger>().score;
+        int scoreFour = GameObject.Find("Point 4 Trigger").GetComponent<PointTrigger>().score;
+
+        for (int i = 0; i < 6; i++)
+        {
+            if (scoreOne >= 1 && scoreTwo >= 1 && scoreThree >= 1 && scoreFour >= 1)
+            {
+                score += 20;
+                scoreOne--;
+                scoreTwo--;
+                scoreThree--;
+                scoreFour--;
+            }
+        }
+        score += scoreOne + (scoreTwo * 2) + (scoreThree * 3) + (scoreFour * 4);
+        txt.text = score.ToString();
+    }
+}
---
+++ Assets/Scripts/Score/PointTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointTrigger : MonoBehaviour
+{
+    public int score;
+    private void OnTriggerEnter(Collider other)
+    {
+        score++;
+        Debug.Log("scored" + score);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        score--;
+    }
+}
