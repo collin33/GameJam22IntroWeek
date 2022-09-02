b37569c8c6a1616f7883aa523d923d918f7a4c3e
Collin33 <meinderscollin@gmail.com>
2022-09-02 04:44:04 +0200

    Aim rework Pt1

--- Assets/Scripts/PuckPlacer.cs
+++ Assets/Scripts/PuckPlacer.cs
@@ -16,8 +16,8 @@ public class PuckPlacer : MonoBehaviour
     public Rigidbody currentPuck;
     [SerializeField] private float moveSpeed = 50;
 
-    private readonly Vector3 _cameraStartPosition = new Vector3(11, 1.5f, 0.1f);
-    private readonly Vector3 _cameraLookAt = new Vector3(-9.972f, 0.1885f, 0.0234f);
+    private readonly Vector3 _cameraStartPosition = new Vector3(11, 2f, 0.1f);
+    private readonly Vector3 _cameraLookAt = new Vector3(25f, -90f, 0f);
     
     private bool _isPlacing = true;
     private bool _isShooting;
@@ -33,7 +33,7 @@ public class PuckPlacer : MonoBehaviour
         backAction.Enable();
         
         _cam.transform.position = _cameraStartPosition;
-        _cam.transform.LookAt(_cameraLookAt);
+        _cam.transform.rotation =  UnityEngine.Quaternion.Euler(_cameraLookAt) ;
     }
 
     private void Update()
@@ -42,36 +42,52 @@ public class PuckPlacer : MonoBehaviour
 
         if (_isPlacing && currentPuck)
         {
+            //STATE A: GENERAL VIEW, ALLOW FOR PLACING
+            //Debug.Log("State A");
+
             currentPuck.velocity = new Vector3(-moveDirection.y, currentPuck.velocity.y, moveDirection.x);
+
         } else if (_isShooting && currentPuck)
         {
+            // STATE B: CAMERA ADJUSTED, ALLOW FOR AIMING
+            //Debug.Log("State B");
+
             var puckTransForm = currentPuck.transform;
 
             Vector3 position = new Vector3();
-            position = puckTransForm.forward + puckTransForm.position + new Vector3(0, 1, 0);
+            position = puckTransForm.position + new Vector3(1.5f, 1, 0);
 
             _cam.transform.position = position;
-            _cam.transform.LookAt(_cameraLookAt);
+            _cam.transform.rotation =  UnityEngine.Quaternion.Euler(_cameraLookAt) ;
+
+            if (enterAction.triggered){
+                currentPuck.velocity=new Vector3(-transform.position.z,0,0)*20;
+            }
+
         }
         else
         {
+            //SAFE SCENARIO, DEBUGS IN CASE SOMETHING UNINTENDED HAPPENS
             Debug.Log(_isPlacing);
             Debug.Log(currentPuck);
         }
 
         if (enterAction.triggered && _isPlacing && !_isShooting)
         {
+            //STATE UPDATER, CHANGE FROM STATE A TO B
             _isPlacing = false;
             _isShooting = true;
+            currentPuck.velocity=new Vector3(0,0,0);
         }
 
         if (backAction.triggered && _isShooting && !_isPlacing)
         {
+            //STATE UPDATER, REVERT FROM STATE B TO A
             _isPlacing = true;
             _isShooting = false;
 
             _cam.transform.position = _cameraStartPosition;
-            _cam.transform.LookAt(_cameraLookAt);
+            _cam.transform.rotation =  UnityEngine.Quaternion.Euler(_cameraLookAt) ;
         }
 
     }
